3fc4c12dd2fcab8e319586f6ecebe17e0f6b0b91
Sebastian Lettner <sebastian.lettner@unternehmertum.de>
2020-10-13 10:54:08 +0200

    added tests and documentation for conversion module

---
+++ tests/data_access/test_conversion.py
@@ -0,0 +1,38 @@
+from pytest import mark
+import numpy as np
+
+from data_access.conversions import (
+    to_json_compatible_value,
+    to_json_compatible_dict,
+)
+
+
+@mark.parametrize(
+    ["value_to_convert", "expected_value"],
+    [
+        (np.array([1, 2, 3]), [1, 2, 3]),
+        (np.array([[1, 2], [3, 4]]), [[1, 2], [3, 4]]),
+        (np.array([1.0, 2.0, 3.0]), [1.0, 2.0, 3.0]),
+        (np.int64(10), 10),
+        (np.float(10.5), 10.5),
+        (np.float32(10.5), 10.5),
+        (np.float64(10.5), 10.5),
+        (np.int32(1), 1),
+        (np.int64(2), 2),
+        (np.uint32(3), 3),
+        (np.inf, "inf"),
+        (np.nan, "nan"),
+    ],
+)
+def test_to_json_compatible_value(value_to_convert, expected_value):
+    assert to_json_compatible_value(value_to_convert) == expected_value
+
+
+@mark.parametrize(
+    ["dict_to_convert", "expected_dict"],
+    [
+        ({"a": np.int32(1), "b": np.array([1, 2])}, {"a": 1, "b": [1, 2]}),
+    ],
+)
+def test_to_json_compatible_dict(dict_to_convert, expected_dict):
+    assert to_json_compatible_dict(dict_to_convert) == expected_dict
