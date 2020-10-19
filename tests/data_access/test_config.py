14891e225be44aadd862dca2ed22a0230f18c2bc
Adrian Rumpold <a.rumpold@gmail.com>
2020-10-19 14:08:22 +0200

    Do not leak pw/secret from config objects
    Includes regression test for issue #6.

---
+++ tests/data_access/test_config.py
@@ -0,0 +1,30 @@
+from data_access.remote_storage import RemoteStorageConfig
+from data_access.sql_access import DatabaseConfig
+
+
+def test_db_config_repr_does_not_include_pw():
+    """
+    Ensure that str representation of database config does not leak password.
+
+    Regression test for issue #6.
+    """
+    cfg = DatabaseConfig(
+        "host", "name", "user", "1234", pw="secretpass", log_statements=False
+    )
+
+    assert cfg.pw not in repr(cfg)
+    assert cfg.pw not in str(cfg)
+
+
+def test_storage_config_repr_does_not_include_secret():
+    """
+    Ensure that str representation of storage config does not leak secret.
+
+    Regression test for issue #6.
+    """
+    cfg = RemoteStorageConfig(
+        "provider", "key", "bucket", "secretkey", "region", "host", "1234", "base_path"
+    )
+
+    assert cfg.secret not in repr(cfg)
+    assert cfg.secret not in str(cfg)
