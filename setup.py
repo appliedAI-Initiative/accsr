471bab1ba10dc5c8d703974ba063b2852a7c58c8
Gitlab CI <gitlab@gitlab.aai.lab>
2020-12-09 10:52:38 +0000

    Bump version: 0.1.8-dev1 → 0.1.8-dev2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    version="0.1.8-dev1",
+    version="0.1.8-dev2",
     description="Utils for accessing data from anywhere",
     install_requires=open("requirements.txt").readlines(),
     setup_requires=["wheel"],
