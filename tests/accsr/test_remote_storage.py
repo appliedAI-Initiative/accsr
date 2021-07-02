6dc0f425a32873bd2ec9a6328713c33ca34afc37
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2021-07-02 14:13:35 +0200

    Add fixtures to improve setup and teardown in remote_storage tests

--- tests/accsr/test_remote_storage.py
+++ tests/accsr/test_remote_storage.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import Generator
 
 import pytest
 
@@ -9,6 +10,28 @@ def change_to_resources_dir(test_resources, request):
     os.chdir(test_resources)
     yield
     os.chdir(request.config.invocation_dir)
+
+
+@pytest.fixture()
+def test_filename(
+    change_to_resources_dir, storage, request
+) -> Generator[str, None, None]:
+    """Pushes a file to remote storage, yields its filename and then deletes it from remote storage"""
+    filename = request.param
+    storage.push_file(filename)
+    yield filename
+    storage.delete(filename)
+
+
+@pytest.fixture()
+def test_dirname(
+    change_to_resources_dir, storage, request
+) -> Generator[str, None, None]:
+    """Pushes a directory to remote storage, yields its name and then deletes it from remote storage"""
+    dirname = request.param
+    storage.push_directory(dirname)
+    yield dirname
+    storage.delete(dirname)
 
 
 def test_delete_no_matches(storage, caplog):
@@ -44,55 +67,67 @@ def test_delete_dir(storage):
     assert len(storage.list_objects("sample_dir")) == 0
 
 
-# TODO: improve setup and cleanup for tests involving pushing
-
-
-def test_push_file_empty_base_path(storage):
-    storage.delete("sample.txt")
-    remote_objects = storage.push("sample.txt")
+@pytest.mark.parametrize(
+    "test_filename",
+    ["sample.txt"],
+    indirect=["test_filename"],
+)
+def test_push_file_empty_base_path(storage, test_filename):
+    remote_objects = storage.push(test_filename)
     assert len(remote_objects) == 1
     # we need lstrip because s3 paths (and names) start with "/" while google storage paths start without it...
-    assert remote_objects[0].name.lstrip("/") == "sample.txt"
-    storage.delete("sample.txt")
+    assert remote_objects[0].name.lstrip("/") == test_filename
 
 
-def test_push_file_nonempty_base_path(storage):
+@pytest.mark.parametrize(
+    "test_filename",
+    ["sample.txt"],
+    indirect=["test_filename"],
+)
+def test_push_file_nonempty_base_path(storage, test_filename):
     base_path = "base_path"
     storage.set_remote_base_path(base_path)
-    storage.delete("sample.txt")
-    remote_objects = storage.push("sample.txt")
+    remote_objects = storage.push(test_filename)
     assert len(remote_objects) == 1
-    assert remote_objects[0].name.lstrip("/") == f"{base_path}/sample.txt"
-    storage.delete("sample.txt")
+    assert remote_objects[0].name.lstrip("/") == f"{base_path}/{test_filename}"
 
 
-def test_push_directory(storage):
-    storage.delete("sample_dir")
-    remote_objects = storage.push("sample_dir")
+@pytest.mark.parametrize(
+    "test_dirname",
+    ["sample_dir"],
+    indirect=["test_dirname"],
+)
+def test_push_directory(storage, test_dirname):
+    remote_objects = storage.push(test_dirname)
     assert len(remote_objects) == 2
-    assert len(storage.list_objects("sample_dir")) == 2
-    storage.delete("sample_dir")
+    assert len(storage.list_objects(test_dirname)) == 2
 
 
-def test_pull_file(storage, tmpdir):
+@pytest.mark.parametrize(
+    "test_filename",
+    ["sample.txt"],
+    indirect=["test_filename"],
+)
+def test_pull_file(storage, test_filename, tmpdir):
     local_base_dir = tmpdir.mkdir("remote_storage")
-    storage.push("sample.txt")
-    storage.pull("sample.txt", local_base_dir=local_base_dir)
-    assert os.path.isfile(os.path.join(local_base_dir, "sample.txt"))
-    pulled_files = storage.pull("sample.txt")
+    storage.pull(test_filename, local_base_dir=local_base_dir)
+    assert os.path.isfile(os.path.join(local_base_dir, test_filename))
+    pulled_files = storage.pull(test_filename)
     assert pulled_files == []
-    storage.delete("sample.txt")
 
 
-def test_pull_dir(storage, tmpdir):
+@pytest.mark.parametrize(
+    "test_dirname",
+    ["sample_dir"],
+    indirect=["test_dirname"],
+)
+def test_pull_dir(storage, test_dirname, tmpdir):
     local_base_dir = tmpdir.mkdir("remote_storage")
-    storage.push("sample_dir")
-    storage.pull("sample_dir", local_base_dir=local_base_dir)
-    assert os.path.isdir(os.path.join(local_base_dir, "sample_dir"))
-    assert len(os.listdir(os.path.join(local_base_dir, "sample_dir"))) == 2
-    pulled_files = storage.pull("sample_dir")
+    storage.pull(test_dirname, local_base_dir=local_base_dir)
+    assert os.path.isdir(os.path.join(local_base_dir, test_dirname))
+    assert len(os.listdir(os.path.join(local_base_dir, test_dirname))) == 2
+    pulled_files = storage.pull(test_dirname)
     assert pulled_files == []
-    storage.delete("sample_dir")
 
 
 # TODO or not TODO: many cases are missing - pulling/pushing nonexisting files, checking names, testing overwriting.
