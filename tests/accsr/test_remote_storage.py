669350d338bbc9ace57b157fe28ea9cf4b67648f
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2021-07-02 10:29:35 +0200

    Set change_to_resources_dir fixture to autouse to simplify test signatures

--- tests/accsr/test_remote_storage.py
+++ tests/accsr/test_remote_storage.py
@@ -4,7 +4,7 @@ import os
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module", autouse=True)
 def change_to_resources_dir(test_resources, request):
     os.chdir(test_resources)
     yield
@@ -18,7 +18,7 @@ def test_delete_no_matches(storage, caplog):
     assert "Not deleting anything" in caplog.text
 
 
-def test_delete_file(storage, change_to_resources_dir):
+def test_delete_file(storage):
     storage.push_file("sample.txt", overwrite_existing=True)
     assert len(storage.list_objects("sample.txt")) == 1
     deleted_objects = storage.delete("sample.txt")
@@ -26,7 +26,7 @@ def test_delete_file(storage, change_to_resources_dir):
     assert len(storage.list_objects("sample.txt")) == 0
 
 
-def test_delete_with_base_path(storage, change_to_resources_dir):
+def test_delete_with_base_path(storage):
     base_path = "base_path"
     storage.set_remote_base_path(base_path)
     storage.push_file("sample.txt", overwrite_existing=True)
@@ -36,7 +36,7 @@ def test_delete_with_base_path(storage, change_to_resources_dir):
     assert deleted_objects[0].name == f"{base_path}/sample.txt"
 
 
-def test_delete_dir(storage, change_to_resources_dir):
+def test_delete_dir(storage):
     storage.push_directory("sample_dir", overwrite_existing=True)
     assert len(storage.list_objects("sample_dir")) == 2
     deleted_objects = storage.delete("sample_dir")
@@ -47,7 +47,7 @@ def test_delete_dir(storage, change_to_resources_dir):
 # TODO: improve setup and cleanup for tests involving pushing
 
 
-def test_push_file_empty_base_path(storage, change_to_resources_dir):
+def test_push_file_empty_base_path(storage):
     storage.delete("sample.txt")
     remote_objects = storage.push("sample.txt")
     assert len(remote_objects) == 1
@@ -56,7 +56,7 @@ def test_push_file_empty_base_path(storage, change_to_resources_dir):
     storage.delete("sample.txt")
 
 
-def test_push_file_nonempty_base_path(storage, change_to_resources_dir):
+def test_push_file_nonempty_base_path(storage):
     base_path = "base_path"
     storage.set_remote_base_path(base_path)
     storage.delete("sample.txt")
@@ -66,7 +66,7 @@ def test_push_file_nonempty_base_path(storage, change_to_resources_dir):
     storage.delete("sample.txt")
 
 
-def test_push_directory(storage, change_to_resources_dir):
+def test_push_directory(storage):
     storage.delete("sample_dir")
     remote_objects = storage.push("sample_dir")
     assert len(remote_objects) == 2
@@ -74,7 +74,7 @@ def test_push_directory(storage, change_to_resources_dir):
     storage.delete("sample_dir")
 
 
-def test_pull_file(storage, change_to_resources_dir, tmpdir):
+def test_pull_file(storage, tmpdir):
     local_base_dir = tmpdir.mkdir("remote_storage")
     storage.push("sample.txt")
     storage.pull("sample.txt", local_base_dir=local_base_dir)
@@ -84,7 +84,7 @@ def test_pull_file(storage, change_to_resources_dir, tmpdir):
     storage.delete("sample.txt")
 
 
-def test_pull_dir(storage, change_to_resources_dir, tmpdir):
+def test_pull_dir(storage, tmpdir):
     local_base_dir = tmpdir.mkdir("remote_storage")
     storage.push("sample_dir")
     storage.pull("sample_dir", local_base_dir=local_base_dir)
