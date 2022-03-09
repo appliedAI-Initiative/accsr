9d19ce64e094bb42ac7dc4d3608f688e1480644c
slettner <sebastian.lettner24@gmail.com>
2022-03-09 11:04:36 +0100

    fix: indent error

--- src/accsr/remote_storage.py
+++ src/accsr/remote_storage.py
@@ -601,7 +601,7 @@ class RemoteStorage:
                 relative_obj_path = self._get_relative_remote_path(obj)
                 if not path_regex.match(relative_obj_path):
                     log.debug(f"Skipping {relative_obj_path} due to regex {path_regex}")
-                skip = True
+                    skip = True
 
             else:
                 local_path = self._get_destination_path(obj, local_base_dir)
