c322cc360a0b2070dc53f342f9246568da162d49
Michael Panchenko <michael.panchenko@unternehmertum.de>
2023-02-24 11:40:35 +0100

    Bump version: 0.4.1 → 0.4.2-dev0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    version="0.4.1",
+    version="0.4.2-dev0",
     description="Utils for accessing data from anywhere",
     install_requires=open("requirements.txt").readlines(),
     setup_requires=["wheel"],
